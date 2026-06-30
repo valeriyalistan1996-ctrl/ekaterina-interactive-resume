751e807a208b3f9f21fcbf1addae380f0aad69b6
valeriyalistan1996-ctrl <valeriyalistan1996@gmail.com>
2026-06-30 16:02:40 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__RESUME_CONFIG__ = {
+  "supabaseUrl": "https://gzurzrcvdajnlxupbpos.supabase.co",
+  "supabaseAnonKey": "sb_publishable_zBsEFrdRj8nb3K63sWTBlg_aLsEXDAR",
+  "slug": "main"
+};
